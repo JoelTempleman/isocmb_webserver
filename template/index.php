bfe11589a7d00cfe0006be9ae41d503baf27fd9c
Joel Templeman <59157736+JoelTempleman@users.noreply.github.com>
2020-12-24 18:18:15 -0600

    Update index.php

--- template/index.php
+++ template/index.php
@@ -4,10 +4,12 @@
  * @subpackage ISOC theme created by Occhio Web Development
  */
 
-get_header();
+echo phpinfo();
 
-get_template_part( 'content', (get_post_format()) ? get_post_format() : get_post_type()  );
+//get_header();
+
+//get_template_part( 'content', (get_post_format()) ? get_post_format() : get_post_type()  );
 
 // get_sidebar();
 
-get_footer();
+//get_footer();
